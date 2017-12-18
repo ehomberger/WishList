de3bad9a8104cc4857ba2155b4f4e23b10508df6
Carson Miller <carsondm@gmail.com>
2017-12-18 14:41:55 -0500

    Form on index page, added /list/add post route

--- Backend/simple_server/src/main.rs
+++ Backend/simple_server/src/main.rs
@@ -1,4 +1,4 @@
-#![feature(plugin)]
+#![feature(plugin, custom_derive)]
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
@@ -6,7 +6,15 @@ extern crate rocket;
 use std::io;
 use std::path::{Path, PathBuf};
 
+use rocket::request::{Form, FromFormValue};
+use rocket::response::Redirect;
 use rocket::response::NamedFile;
+
+#[derive(FromForm)]
+struct ListItem {
+	name: String,
+	price: f32,
+}
 
 #[get("/")]
 fn index() -> io::Result<NamedFile> {
@@ -18,8 +26,16 @@ fn files(file: PathBuf) -> Option<NamedFile> {
 	NamedFile::open(Path::new("static/").join(file)).ok()
 }
 
+#[post("/list/add", data = "<item_form>")]
+fn add_item<'a>(item_form: Form<'a, ListItem>) -> Result<Redirect, String> {
+	let item = item_form.get();
+
+	println!("[item name, item price] --- [{}, {}]", item.name, item.price);
+	Ok(Redirect::to("/"))
+}
+
 fn rocket() -> rocket::Rocket {
-	rocket::ignite().mount("/", routes![index, files])
+	rocket::ignite().mount("/", routes![index, files, add_item])
 }
 
 fn main() {
